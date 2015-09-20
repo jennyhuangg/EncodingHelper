63283fa656fdae90a15364d9dd597788ecb7a4f2
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-09-20 15:07:40 -0400

    empty methods provided by wizard.

---
+++ edu/andover/jhuang/EncodingHelperCharTest.java
@@ -0,0 +1,14 @@
+package edu.andover.jhuang;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EncodingHelperCharTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
