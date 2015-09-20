bbf9c85975a0ca8efd128b721c9cd0c8893b8c3b
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-09-20 15:05:12 -0400

    empty methods provided by wizard.

--- edu/andover/jhuang/EncodingHelperChar.java
+++ edu/andover/jhuang/EncodingHelperChar.java
@@ -1,5 +1,96 @@
 package edu.andover.jhuang;
 
+/**
+ * The main model class for the EncodingHelper project in Software Design,
+ * Fall 2015, Phillips Academy.
+ * 
+ * Each object of this class represents a single Unicode character.  The class's
+ * methods generate various representations of the character.
+ * 
+ * Every method that takes an argument may throw an IllegalArgumentException if
+ * given an illegal input.  This includes codepoints that are out of the legal
+ * range for Unicode, byte arrays that are invalid encodings for a single
+ * character, etc.
+ * 
+ * @author Jadrian Miles
+ */
 class EncodingHelperChar {
-
+    private int codepoint;
+    
+    public EncodingHelperChar(int codepoint) {
+        // Not yet implemented.
+    }
+    
+    public EncodingHelperChar(byte[] utf8Bytes) {
+        // Not yet implemented.
+    }
+    
+    public EncodingHelperChar(char ch) {
+        // Not yet implemented.
+    }
+    
+    public int getCodepoint() {
+        // Not yet implemented.
+        return -1;
+    }
+    
+    public void setCodepoint(int codepoint) {
+        // Note yet implemented.
+    }
+    
+    /**
+     * Converts this character into an array of the bytes in its UTF-8
+     * representation.
+     *   For example, if this character is a lower-case letter e with an acute
+     * accent, whose UTF-8 form consists of the two-byte sequence C3 A9, then
+     * this method returns a byte[] of length 2 containing C3 and A9.
+     * 
+     * @return the UTF-8 byte array for this character
+     */
+    public byte[] toUtf8Bytes() {
+        // Not yet implemented.
+        return null;
+    }
+    
+    /**
+     * Generates the conventional 4-digit hexadecimal code point notation for
+     * this character.
+     *   For example, if this character is a lower-case letter e with an acute
+     * accent, then this method returns the string U+00E9 (no quotation marks in
+     * the returned String).
+     *
+     * @return the U+ string for this character
+     */
+    public String toCodepointString() {
+        // Not yet implemented.
+        return "";
+    }
+    
+    /**
+     * Generates a hexadecimal representation of this character suitable for
+     * pasting into a string literal in languages that support hexadecimal byte
+     * escape sequences in string literals (e.g. C, C++, and Python).
+     *   For example, if this character is a lower-case letter e with an acute
+     * accent (U+00E9), then this method returns the string \xC3\xA9. Note that
+     * quotation marks should not be included in the returned String.
+     *
+     * @return the escaped hexadecimal byte string
+     */
+    public String toUtf8String() {
+        // Not yet implemented.
+        return "";
+    }
+    
+    /**
+     * Generates the official Unicode name for this character.
+     *   For example, if this character is a lower-case letter e with an acute
+     * accent, then this method returns the string "LATIN SMALL LETTER E WITH
+     * ACUTE" (without quotation marks).
+     *
+     * @return this character's Unicode name
+     */
+    public String getCharacterName() {
+        // Not yet implemented.
+        return "";
+    }
 }
