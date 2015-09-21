1c7a17446a4998a433f71ec6013c489f94473bdc
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-09-21 15:28:21 -0400

    Populated EncodingHelperChar with methods.

--- edu/andover/jhuang/EncodingHelperCharTest.java
+++ edu/andover/jhuang/EncodingHelperCharTest.java
@@ -7,8 +7,31 @@ import org.junit.Test;
 public class EncodingHelperCharTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCharConstructorShouldRepresentCorrectCodepoint() {
+		EncodingHelperChar c = new EncodingHelperChar(10295); //choose trickier number
+		int outputCodePoint = c.getCodepoint();
+		assertEquals("Did not make codepoint correctly", 10295, outputCodePoint);
 	}
+	
+	@Test
+	public void illegalCharConstructorShouldThrowWhenCodepointIsNegative() {
+		 try {
+				EncodingHelperChar c = new EncodingHelperChar(-1);
+		        fail("My constructor didn't throw when the codepoint was negative");
+		 } catch(IllegalArgumentException expectedException) {
+		        // No action needed.
+		 }
+	}
+	
+	@Test
+	public void illegalCharConstructorShouldThrowWhenCodepointIsTooLarge() {
+		try {
+				EncodingHelperChar c = new EncodingHelperChar(0x110000);
+				fail("My constructor didn't throw when the codepoint was too large");
+		} catch (IllegalArgumentException e) {
+				
+		}
+	}
+	
 
 }
