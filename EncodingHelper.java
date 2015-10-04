07466fc570f43fbbebfe564d004aaae03aa1c918
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-10-04 02:02:29 -0400

    Added new class

---
+++ EncodingHelper.java
@@ -0,0 +1,10 @@
+import edu.andover.jhuang.*;
+
+public class EncodingHelper {
+
+	public static void main(String[] args) {
+		EncodingHelperParser ehp = new EncodingHelperParser(args[0]);
+		System.out.println("String: " + args[0]);
+		System.out.println("Codepoints: " + ehp.toCodepointString)
+	}
+}
