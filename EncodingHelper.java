43632c30c2792e395a440c835d5d5d1979593398
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-10-05 01:22:56 -0400

    Edited main method

--- EncodingHelper.java
+++ EncodingHelper.java
@@ -3,8 +3,6 @@ import edu.andover.jhuang.*;
 public class EncodingHelper {
 
 	public static void main(String[] args) {
-		EncodingHelperParser ehp = new EncodingHelperParser(args[0]);
-		System.out.println("String: " + args[0]);
-		System.out.println("Codepoints: " + ehp.toCodepointString)
+		System.out.println(EncodingHelperApp.runApp(args));
 	}
 }
