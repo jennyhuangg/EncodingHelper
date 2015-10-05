dcd81033aa7c9997dc2ca420c402e1d408c34741
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-10-05 02:21:05 -0400

    Converted to ArrayList and fixed bugs

--- edu/andover/jhuang/EncodingHelperString.java
+++ edu/andover/jhuang/EncodingHelperString.java
@@ -1,22 +1,23 @@
 package edu.andover.jhuang;
+import java.util.ArrayList;
 
 public class EncodingHelperString {
 
-private int codepoints[];
+private ArrayList<Integer> codepoints = new ArrayList<Integer>();
 
 	public EncodingHelperString(String inputString) {
 		for (int i = 0; i < inputString.length(); i++)
 		{
 			char ch = inputString.charAt(i);
-	        char[] c = {ch};
-	        codepoints[i] = Character.codePointAt(c, 0);
-		}		
+			EncodingHelperChar ehc = new EncodingHelperChar(ch);
+	        codepoints.add(ehc.getCodepoint());
+		}
 	}
 	
 	public String toCodepointString() {
 		String s = "";
-		for (int i = 0; i < codepoints.length; i++) {
-			EncodingHelperChar c = new EncodingHelperChar(codepoints[i]);
+		for (int i = 0; i < codepoints.size(); i++) {
+			EncodingHelperChar c = new EncodingHelperChar(codepoints.get(i));
 			s += c.toCodepointString() + " ";
 		}
 		return s;
@@ -24,8 +25,8 @@ private int codepoints[];
 	
 	public String toUtf8String() {
 		String s = "";
-		for (int i = 0; i < codepoints.length; i++) {
-			EncodingHelperChar c = new EncodingHelperChar(codepoints[i]);
+		for (int i = 0; i < codepoints.size(); i++) {
+			EncodingHelperChar c = new EncodingHelperChar(codepoints.get(i));
 			s += c.toUtf8String();
 		}
 		return s;
