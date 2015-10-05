361164a47aa374273163b232e65cfbda206cd326
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-10-05 03:56:08 -0400

    Added int array list constructor

--- edu/andover/jhuang/EncodingHelperString.java
+++ edu/andover/jhuang/EncodingHelperString.java
@@ -1,17 +1,34 @@
 package edu.andover.jhuang;
 import java.util.ArrayList;
 
+/*
+ * EncodingHelper Model for String Input
+ * 
+ * Jenny Huang
+ * Project 1.3
+ * COMP-630: Software Design, Instructor: Dr. Miles
+ * 5 October 2015
+ */
+
 public class EncodingHelperString {
 
 private ArrayList<Integer> codepoints = new ArrayList<Integer>();
 
 	public EncodingHelperString(String inputString) {
-		for (int i = 0; i < inputString.length(); i++)
-		{
+		for (int i = 0; i < inputString.length(); i++) {
 			char ch = inputString.charAt(i);
 			EncodingHelperChar ehc = new EncodingHelperChar(ch);
 	        codepoints.add(ehc.getCodepoint());
 		}
+	}
+	
+	public EncodingHelperString(ArrayList<Integer> inputCodepoints) {
+		for (int i = 0; i < inputCodepoints.size(); i ++) {
+			codepoints.add(inputCodepoints.get(i));
+		}
+	}
+	public ArrayList<Integer> getCodepoint() {
+		return codepoints;
 	}
 	
 	public String toCodepointString() {
