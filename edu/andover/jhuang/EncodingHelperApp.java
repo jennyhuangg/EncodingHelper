cf16736c340cf4ff23f85e4315a67aa544e6b4d2
Jenny Huang <jhuang@Jennys-MBP.andover.edu>
2015-10-05 02:20:38 -0400

    Added more input/output cases

--- edu/andover/jhuang/EncodingHelperApp.java
+++ edu/andover/jhuang/EncodingHelperApp.java
@@ -9,8 +9,20 @@ public class EncodingHelperApp {
 		String inputType = getInputType(args);
 		String outputType = getOutputType(args);
 		
-		//if character
-		if (data.size() == 1 && data.get(0).length() == 1 && 
+		//help message
+		if (data.get(0).equals("--help") || data.get(0).equals("-h")) {
+			//got help from cam about the help message
+			return "Usage: java EncodingHelper [-i type] [-o type] <data>\n" +
+                "\tInput types: string, utf8, codepoint\n" +
+                "\tOutput types: string, utf8, codepoint, summary";
+		}
+		//no data input from user
+		else if (data.size() == 0) {
+			return "need an input arg: please input a string, "
+					+ "utf8 sequence, or codepoint(s)";
+		}
+		//character input with summary output
+		else if (data.size() == 1 && data.get(0).length() == 1 && 
 				inputType.equals("string")) {
 			 char c = data.get(0).charAt(0);
 			 EncodingHelperChar ehc = new EncodingHelperChar(c);
@@ -21,11 +33,12 @@ public class EncodingHelperApp {
 			 s += "UTF-8: " + ehc.toUtf8String();
 			 return s;
 		}
-		//if string
+		//string input with summary output
 		else if (data.size() == 1 && data.get(0).length() > 1 && 
 				inputType.equals("string")) {
 			 for (int i = 0; i < args.length; i++) {
-				 EncodingHelperString ehs = new EncodingHelperString(data.get(0));
+				 EncodingHelperString ehs = new 
+						 EncodingHelperString(data.get(0));
 				 String s = "";
 				 s += "String: " + data.get(0) + "\n";
 				 s += "Codepoints: " + ehs.toCodepointString() + "\n";
@@ -33,11 +46,20 @@ public class EncodingHelperApp {
 				 return s; 
 			 }
 		}
-		//if string is more than one arg
+		//string input with summary output is more than one arg
 		else if (inputType.equals("string") && data.size() > 1) {
 			System.out.println("Input cannot be longer than one string");
 		}
-return "";
+		//utf8 input with summary output
+		else if (inputType.equals("utf8") && data.size() == 1) {
+			
+		}
+		//utf8 input with summary output is more than one arg
+		else if (inputType.equals("utf8") && data.size() > 1) {
+			
+		}
+		//codepoint with summary output
+		return "";
 	}
 	
 	//cam helped me come up with the idea of a data list
@@ -102,12 +124,12 @@ return "";
 					return "codepoint";
 				else if (args[i+1].equalsIgnoreCase("summary"))
 					return "summary";
-				}
 				else {
 					System.out.println("Incorrect output type: can only enter "
 							+ "'string' 'utf8' 'codepoint' or 'summary'");
 					System.exit(1);
 				}	
+			}
 		}
 		return "summary";
 	}
